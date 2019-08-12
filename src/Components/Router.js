9fbb1a9819e09c6c8196ddbbd91d9a5e82665145
kyungso <cks678928@gmail.com>
2019-08-12 17:12:50 +0900

    Location aware header using withRouter

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Redirect,
   Switch
@@ -8,14 +8,18 @@ import {
 import Home from "../Routes/Home";
 import TV from "../Routes/TV";
 import Search from "../Routes/Search";
+import Header from "./Header";
 
 export default () => (
   <Router>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/tv" exact component={TV} />
-      <Route path="/search" exact component={Search} />
-      <Redirect from="*" to="/" />
-    </Switch>
+    <>
+        <Header />
+        <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" exact component={TV} />
+        <Route path="/search" exact component={Search} />
+        <Redirect from="*" to="/" />
+        </Switch>
+    </>
   </Router>
 );
