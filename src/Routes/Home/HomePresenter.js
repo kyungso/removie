3d9ec7270eb998134da4835955b5a4cd0d008529
kyungso <cks678928@gmail.com>
2019-08-13 23:53:22 +0900

    Message Component

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 
 import Section from "../../Components/Section";
 import Loader from "../../Components/Loader";
+import Message from "../../Components/Message";
 
 const Container = styled.div`
     padding: 0px 20px;
@@ -37,6 +38,7 @@ const HomePresenter = ({ nowPlaying, upcoming, popular, error, loading }) =>
                     ))}
                 </Section>
             )}
+            {error && <Message color="#e74c3c" text={error} />}
         </Container>
     );
 
