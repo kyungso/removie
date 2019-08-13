2e8beef400b9115a62952c686dbcdc9cdf1b25ac
kyungso <cks678928@gmail.com>
2019-08-13 13:49:00 +0900

    update TVContainer with received datas from tv API

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -22,14 +22,16 @@ export default class extends React.Component {
             const {
                 data: { results: popular }
             } = await moviesApi.popular();
+
             this.setState({
                 nowPlaying,
                 upcoming,
                 popular
             });
+            
         } catch {
             this.setState({
-                error: "Can't find movies information."
+                error: "Can't find movie information."
             });
         } finally {
             this.setState({
--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,20 +1,50 @@
 import React from "react";
 import TVPresenter from "./TVPresenter";
+import { tvApi } from "../../api";
 
 export default class extends React.Component {
     state = {
-        toRated: null,
+        topRated: null,
         popular: null,
         airingToday: null,
         loading: true,
         error: null
     };
 
+    async componentDidMount() {
+        try {
+            const {
+                data: { results: topRated }
+            } = await tvApi.topRated();
+            const {
+                data: { results: popular }
+            } = await tvApi.popular();
+            const {
+                data: { results: airingToday }
+            } = await tvApi.airingToday();
+
+            this.setState({
+                topRated,
+                popular,
+                airingToday
+            });
+            
+        } catch {
+            this.setState({
+                error: "Can't find TV information"
+            });
+        } finally {
+            this.setState({
+                loading: false
+            });
+        }
+    }
+
     render() {
-        const { toRated, popular, airingToday, loading, error } = this.state;
+        const { topRated, popular, airingToday, loading, error } = this.state;
         return (
             <TVPresenter 
-                toRated={toRated}
+                topRated={topRated}
                 popular={popular}
                 airingToday={airingToday}
                 loading={loading}
