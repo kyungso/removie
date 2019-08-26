fe6f0d07e736dd425e01bece64d2f27132814aa5
kyungso <cks678928@gmail.com>
2019-08-26 14:52:30 +0900

    change language

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
     baseURL: "https://api.themoviedb.org/3/",
     params: {
         api_key: "de673ff7f12f1f8ac1034c13231d83e1",
-        language: "en-US"
+        language: "ko-KR"
     }
 });
 
