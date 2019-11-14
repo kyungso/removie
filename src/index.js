2e5d59af37b89c8606b26c1ca3a7f93e5f7b7724
kyungso <cks678928@gmail.com>
2019-11-14 16:05:12 +0900

    combine Root.js/configure.js to index.js (ReactDOM.render, createStore, createSagaMiddleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from './Root';
-import * as serviceWorker from './serviceWorker';
 import 'styles/base.scss';
+import App from 'components/App';
+import * as serviceWorker from './serviceWorker';
+import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer, { rootSaga } from 'store/modules';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+    rootReducer, 
+    composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <HashRouter>
+            <App/>
+        </HashRouter>
+    </Provider>, 
+    document.getElementById('root')
+);
+
 serviceWorker.unregister();
