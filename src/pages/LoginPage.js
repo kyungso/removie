3f338f178cc16f84b6ba0a4d0e9c44a9ce6936de
kyungso <cks678928@gmail.com>
2019-11-15 23:31:46 +0900

    (login) solved abnormal approach

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Redirect } from "react-router-dom";
 import LoginContainer from 'containers/LoginContainer';
 
 const LoginPage = () => {
-    
+    let session_id = localStorage.getItem('session_id');
     return (
-        <LoginContainer/>
+        <>
+        { session_id 
+            ? <Redirect to="/" />
+            : <LoginContainer/>
+        }
+        </>
     );
 };
 
