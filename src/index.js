8b28e5ae5ffcf2301089fdb65b0d67ddbd7fc1bb
kyungso <cks678928@gmail.com>
2019-11-16 11:31:43 +0900

    change App.js path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'styles/base.scss';
-import App from 'components/App';
+import App from 'App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
