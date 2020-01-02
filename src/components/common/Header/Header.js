17432554599ded9f4097e5007a0dbd5bcb55ae82
kyungso <cks678928@gmail.com>
2020-01-02 17:14:27 +0900

    add search icon & change language in header

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from "prop-types";
 import { Link, withRouter } from 'react-router-dom';
 
@@ -12,7 +12,16 @@ import { Dropdown, DropdownButton } from 'react-bootstrap';
 const cx = classNames.bind(styles);
 
 const Header = withRouter(({ location: { pathname }, handleLogout }) => {
+    const [searchFocus, setSearchFocus] = useState(false);
+    const searchRef = useRef(null);
     let username = localStorage.getItem('username');
+
+    useEffect(() => {
+        if(searchFocus) {
+            searchRef.current.focus();
+        }
+    }, [searchFocus]);
+
     return (
         <header className={cx('header')}>
             <div className={cx('left-content')}>
@@ -25,33 +34,44 @@ const Header = withRouter(({ location: { pathname }, handleLogout }) => {
                     >
                         <Link to="/" className={cx('nav_link')} 
                             style={{ color: (pathname === "/" ? `#ffffff` : `#7d7d7d`) }}
-                        >Home</Link>
+                        >홈</Link>
                     </li>
                     <li className={cx('left-content-items')}
                         style={{ borderBottom: (pathname === "/movie" ? `3px solid #3498db` : `3px solid transparent`) }}
                     >
                         <Link to="/movie" className={cx('nav_link')}
                             style={{ color: (pathname === "/movie" ? `#ffffff` : `#7d7d7d`) }}
-                        >Movies</Link>
+                        >영화</Link>
                     </li>
                     <li className={cx('left-content-items')}
                         style={{ borderBottom: (pathname === "/tv" ? `3px solid #3498db` : `3px solid transparent`) }}
                     >
                         <Link to="/tv" className={cx('nav_link')}
                             style={{ color: (pathname === "/tv" ? `#ffffff` : `#7d7d7d`) }}
-                        >TV</Link>
+                        >TV 프로그램</Link>
                     </li>
-                    <li className={cx('left-content-items')}
+                    {/* <li className={cx('left-content-items')}
                         style={{ borderBottom: (pathname.includes("/search") ? `3px solid #3498db` : `3px solid transparent`) }}
                     >
                         <Link to="/search" className={cx('nav_link')}
                             style={{ color: (pathname.includes("/search") ? `#ffffff` : `#7d7d7d`) }}
                         >Search</Link>
-                    </li>
+                    </li> */}
                 </ul>
             </div>
             <div className={cx('right-content')}>
                 <ul className={cx('right-content-list')}>
+                    <li className={cx('custom-search-wrapper')}>
+                      <div id="custom-search" onClick={() => setSearchFocus(!searchFocus)}>
+                        <input type="text" ref={searchRef} className={cx('search-query')} placeholder="Movies, TV Shows, Collections" />
+                        <button type="button">
+                          <span className={cx('icon-search')}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 93.83 93.83"><path d="M39,3.17a36,36,0,0,0,0,72A35.69,35.69,0,0,0,59.36,68.8L85.64,95.08a6.55,6.55,0,0,0,9.27-9.24L68.63,59.56A36,36,0,0,0,39,3.17Zm0,13.09A22.91,22.91,0,1,1,16.09,39.17,22.81,22.81,0,0,1,39,16.26Z" transform="translate(-3 -3.17)"/></svg>
+                          </span>
+                        </button>
+                        <input type="submit" value="Submit" style={{ display: 'none' }} />
+                      </div>
+                    </li>
                 {
                     localStorage.getItem('logged') === 'true' && localStorage.getItem('session_id') !== null
                     ? (<li>
