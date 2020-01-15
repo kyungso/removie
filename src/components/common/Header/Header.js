98ea7b52c7fdace332fba02bfb3272a37283d281
kyungso <cks678928@gmail.com>
2020-01-15 13:47:06 +0900

    로그인 페이지 UI 변경

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -10,12 +10,18 @@ import { Dropdown, DropdownButton } from 'react-bootstrap';
 
 const cx = classNames.bind(styles);
 
-const Header = ({ pathname, handleLogout }) => {
+const Header = ({ pathname, handleLogout, searchTerm, searchUpdateTerm }) => {
     const [handleScroll, setHandleScroll] = useState("");
     const [searchFocus, setSearchFocus] = useState(false);
+    const [loginHeader, setLoginHeader] = useState("");
     const searchRef = useRef(null);
     let username = localStorage.getItem('username');
-     
+
+    useEffect(() => {
+        setLoginHeader(pathname === "/login" ? "displayNone" : "");
+        console.log(loginHeader);
+    }, [pathname, loginHeader]);
+
     useEffect(() => {
         window.addEventListener('scroll', () => setHandleScroll(window.scrollY > 70 ? "black" : ""));
     },[]);
@@ -33,15 +39,15 @@ const Header = ({ pathname, handleLogout }) => {
                     <li>
                         <Link to="/"><img src={logo} className={cx('logo')} alt="logo"/></Link>
                     </li>
-                    <li className={cx('left-content-items')}>
+                    <li className={cx('left-content-items', loginHeader)}>
                         <Link to="/" className={cx(['nav_link', pathname === "/" ? "" : "notActive"])} 
                         >홈</Link>
                     </li>
-                    <li className={cx('left-content-items')}>
+                    <li className={cx('left-content-items', loginHeader)}>
                         <Link to="/movie" className={cx(['nav_link', pathname === "/movie" ? "" : "notActive"])} 
                         >영화</Link>
                     </li>
-                    <li className={cx('left-content-items')}>
+                    <li className={cx('left-content-items', loginHeader)}>
                         <Link to="/tv" className={cx(['nav_link', pathname === "/tv" ? "" : "notActive"])}
                         >TV 프로그램</Link>
                     </li>
@@ -54,17 +60,23 @@ const Header = ({ pathname, handleLogout }) => {
                     </li> */}
                 </ul>
             </div>
-            <div className={cx('right-content')}>
+            <div className={cx('right-content', loginHeader)}>
                 <ul className={cx('right-content-list')}>
                     <li className={cx('right-item')}>
                       <div id="custom-search" onClick={() => setSearchFocus(!searchFocus)}>
-                        <input type="text" ref={searchRef} className={cx('search-query')} placeholder="영화, TV 프로그램, 시리즈 검색" />
+                        <input 
+                            className={cx('search-query')} 
+                            type="text" 
+                            ref={searchRef} 
+                            placeholder="영화, TV 프로그램, 시리즈 검색" 
+                            value={searchTerm}
+                            onChange={searchUpdateTerm}
+                        />
                         <button type="button">
                           <span className={cx('icon-search')}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 93.83 93.83"><path d="M39,3.17a36,36,0,0,0,0,72A35.69,35.69,0,0,0,59.36,68.8L85.64,95.08a6.55,6.55,0,0,0,9.27-9.24L68.63,59.56A36,36,0,0,0,39,3.17Zm0,13.09A22.91,22.91,0,1,1,16.09,39.17,22.81,22.81,0,0,1,39,16.26Z" transform="translate(-3 -3.17)"/></svg>
                           </span>
                         </button>
-                        <input type="submit" value="Submit" style={{ display: 'none' }} />
                       </div>
                     </li>
                 {
