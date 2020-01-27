90e4ec7f334a56ee3d8b940f8c96979d7b055be1
kyungso <cks678928@gmail.com>
2020-01-27 23:55:13 +0900

    이미지 hover 시, 이미지 내용 중 개봉날짜, 평점 추가

--- src/components/common/Slider/Item/Item.js
+++ src/components/common/Slider/Item/Item.js
@@ -36,8 +36,23 @@ const Item = ({ media, index, isTV }) => (
           onMouseLeave={onMouseLeaveSlide}
         >
           <div className={cx('image')}>
-            <img src={`https://image.tmdb.org/t/p/w300${media.backdrop_path}`} alt="poster"/>
-            <div className={cx('image-content')}>{title}</div>
+            <div className={cx('image-backdrop')} 
+                 style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w300${media.backdrop_path})` }}>
+            </div>
+            <div className={cx('image-ShadowTop')}></div>
+            <div className={cx('image-content')}>
+              <div className={cx('content-title')}>{title}</div>
+              <span className={cx('content-item')}>
+                {media.release_date
+                  ? media.release_date.substring(0,4)
+                  : media.first_air_date.substring(0,4)}
+              </span>
+              <span className={cx('content-divider')}>・</span>
+              <span className={cx('content-item')}>
+                ★ {media.vote_average}
+                {/* {media.runtime ? media.runtime : (media.runtime === 0 ? '' : media.episode_run_time[0])} min */}
+              </span>
+            </div>
           </div>
           <div className={cx('image-title')}>
             {title.length > 18 ? `${title.substring(0, 18)}...` : title}
